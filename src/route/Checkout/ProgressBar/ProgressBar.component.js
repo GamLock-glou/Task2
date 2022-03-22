5dc8427bf55f521aa4e0df8d531efe2fe45776df
GamLock-glou <klimenkov.z@mail.ru>
2022-03-22 09:44:24 +0300

    Dinamic Code

--- src/route/Checkout/ProgressBar/ProgressBar.component.js
+++ src/route/Checkout/ProgressBar/ProgressBar.component.js
@@ -3,24 +3,20 @@ import './ProgressBar.style'
 
 class ProgressBar extends PureComponent {
     render() {
-        let checkProgressBar = true;
         const keys = Object.keys(this.props.step);
         const idx = keys.findIndex(key => key === this.props.checkoutStep);
-        const a = 100 / keys.length;
-        console.log(a);
+        const onePontWidth = 100 / keys.length;
         return ( <div className='ProgressBar'>
-            <div className='progress' style={{ width: `${a * (idx+1)}%` }} id='progress'></div>
+            <div className='progress' style={{ width: `${onePontWidth * (idx+1)}%` }} id='progress'></div>
                 {keys.map((key,index) => {
-                    const className = !checkProgressBar ? 'ProgressStep' : 'ProgressStep ProgessStepActive'
-                    if(key === this.props.checkoutStep)
-                        checkProgressBar = false;
+                    const className = index>=idx+1 ? 'ProgressStep' : 'ProgressStep ProgessStepActive'
                     if(index !== keys.length-1)
                         return <div 
                             key={index}
-                            style={{ left: `${a * (index + 1)}%` }}
+                            style={{ left: `${onePontWidth * (index + 1)}%` }}
                             className={className} 
                             data-title={this.props.step[key].title.value}>
-                                {!checkProgressBar ? index+1: `✓`}
+                                {index>=idx ? index+1: `✓`}
                             </div>
                     
                 })}
